ed4e3740a7dbba04cbdf2fc73dc18e81630592ae
Jaime García <elgambitero@gmail.com>
2016-06-19 19:00:44 +0200

    Adapt the layout for the new protocoder

--- main.js
+++ main.js
@@ -6,58 +6,45 @@
 */
 
 
+var camera = ui.addCameraView("back", 0, 0, 0.5, 0.375);
 
-
-var dataLabel = ui.addText("data : ",10, 20, 500, 100);
-
-var serial; 
+var arduino
 
 //start connexion with arduino
-ui.addButton("START", 50, 50, 600,100).onClick(function() { 
-	
-	//show arduino incoming data
-    serial = boards.connectSerial(115200, function(connected) {
+ui.addButton("START", 0.0, 0.61, 0.5,0.07).onClick(function() { 
+    arduino= boards.connectArduino(115200, "\r\n", function(connected) {
     	console.log("connected " + connected);
 	});
-
-	serial.onNewData(function(data) {
-		dataLabel.setText("Data : "+ data);
-		console.log(data);
-		media.textToSpeech("tick"); 
-	});
-
 });
 
-
-//write to the serial led on
-ui.addButton("HOME AXIS", 50, 150, 600,100).onClick(function(){ 
+ui.addButton("HOME AXIS", 0.0, 0.68, 0.5,0.07).onClick(function(){ 
 	serial.write("$h\r\n"); 
+	console.log("ok");
 });
 
-ui.addButton("X-", 240, 400, 100,100).onClick(function(){
+
+ui.addButton("STOP", 0.00, 0.75, 0.5, 0.07).onClick(function(){
+	serial.stop(); 
+});
+
+
+ui.addButton("X-", 0.6, 0.68, 0.14,0.07).onClick(function(){
 	serial.write("G91\r\n");
 	serial.write("G1 X-5 F1000");
 });
 
-ui.addButton("X+", 380, 400, 100,100).onClick(function(){
+ui.addButton("X+", 0.8, 0.68, 0.14,0.07).onClick(function(){
 	serial.write("G91\r\n");
 	serial.write("G1 X5 F1000");
 });
 
-ui.addButton("Y+", 310, 300, 100,100).onClick(function(){
+ui.addButton("Y+", 0.7, 0.63, 0.14,0.07).onClick(function(){
 	serial.write("G91\r\n");
 	serial.write("G1 Y5 F1000");
 });
 
-ui.addButton("Y-", 310, 500, 100,100).onClick(function(){
+ui.addButton("Y-", 0.7, 0.73, 0.14,0.07).onClick(function(){
 	serial.write("G91\r\n");
 	serial.write("G1 Y-5 F1000");
 });
-
-ui.addButton("STOP", 50, 600, 600,100).onClick(function(){
-	serial.stop(); 
-});
-
-var camera = ui.addCameraView("back", 0, 700, 360, 480);
-
 
